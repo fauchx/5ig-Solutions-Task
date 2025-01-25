a0dc751bea3cd914063481b06c9ff778b7337e3c
fauchx <fabian.urrutia@correounivalle.edu.co>
2025-01-25 02:48:35 -0500

    Inicio creacion aplicacion punto 3

---
+++ punto3/rickmorty/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <>
+    <h1 className="text-center bg-black text-white">Hola Mundo!
+    </h1>
+    </>
+  );
+}
+
+export default App;
